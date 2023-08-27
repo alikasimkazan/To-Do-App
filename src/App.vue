d2f86585594d54b8cbf383662c858ffca184b15b
Ali Kasım <123629310+alikasimmkazan@users.noreply.github.com>
2023-08-27 23:59:28 +0300

    To Do App
    Vue3 ile To Do list uygulamasi.

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <NavBar />
+  <router-view />
+</template>
+
+<script>
+import NavBar from "./components/NavBar.vue"
+
+export default {
+  components: { NavBar }
+
+
+}
+</script>
+
+<style>
+body {
+  background-color: #f2f2f2;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin: 0 auto;
+  max-width: 600px;
+  color: #555;
+}
+</style>
